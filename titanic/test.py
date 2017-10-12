33f18541cb67049960b34edba0ce3a7fd74e2eaf
huangyaodong <huangyaodong@meituan.com>
2017-10-12 22:04:36 +0800

    update

--- titanic/test.py
+++ titanic/test.py
@@ -9,12 +9,19 @@ sex_map = {'mail': 1, 'female': 2}
 
 def correct_data(df):
     df = df[features]
-    df['family'] = df['Parch'] + df['SibSp']
-    df['Embarked'] = df['Embarked'].map(embark_map).as_type(int)
+    df['family'] = df['Parch'] + df['SibSp'] + 1
     df['Sex'] = df['Sex'].map(sex_map).as_type(int)
+    filled_embark = df['Embarked'].dropna().mode()[0]
+    df['Embarked'] = df['Embarked'].fillna(filled_embark)
+    df['Embarked'] = df['Embarked'].map(embark_map).as_type(int)
+    filled_fare = df['Fare'].dropna().mode()[0]
+    df['Fare'] = df['Fare'].fillna(filled_fare)
     return df
 
 
 train_df = pd.read_csv('input/train.csv')
-train_df = correct_data(train_df)
+test_df = pd.read_csv('input/test.csv')
+test_df.info()
+# train_df = correct_data(train_df)
+
 
